9dc49e1706121fcd2ba0e10131dc04e3611f7ef7
Max Darling <darlingm113@northwood.edu>
2025-12-07 00:21:15 -0500

    Change Router from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
